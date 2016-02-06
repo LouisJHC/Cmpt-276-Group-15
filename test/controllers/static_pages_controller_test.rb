7a4598a6fe07eac91d520b1e51f7c65727aa35a6
Rafiq Dandoo <rafiq.dandoo@gmail.com>
2016-02-06 21:23:04 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | IFitness"
+    assert_select "title", "IFitness"
   end
 
   test "should get help" do
