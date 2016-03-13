3d33da79ae893c5c2741bee26345fbe6a8222052
louisjhc <lcha@sfu.ca>
2016-03-13 05:32:55 +0000

    Added comment section and some design changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
+  has_many :comments
   attr_accessor :remember_token
   
   before_save { self.email = email.downcase }
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310031333) do
+ActiveRecord::Schema.define(version: 20160313042941) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "email"
   end
 
   create_table "users", force: :cascade do |t|
