a76d263a6227d37ae6b05ff18c529bc831902022
louisjhc <lcha@sfu.ca>
2016-03-27 00:40:41 +0000

    Only original commenter can edit/delete his/her post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-	
+
 	include SessionsHelper
         helper_method :current_user
     def show
@@ -24,28 +24,31 @@ class CommentsController < ApplicationController
 	end
 
 	def update
-	  if current_user == @comments.user
+	  
 			@post = Post.find(params[:post_id])
 			@comment = @post.comments.find(params[:id])
-	
-			if @comment.update(params[:comment].permit(:comment))
-				redirect_to post_path(@post)
-			else
-				render 'edit'
-			end
-	  else
-	  	redirect_to root_path
-	  end
-	end
+			
+			if current_user == @comment.user
+		
+				if @comment.update(params[:comment].permit(:comment))
+					redirect_to post_path(@post)
+				else
+					render 'edit'
+				end
+		  else
+		  	redirect_to posts_path
+		  end
+		end
 
 	def destroy
-	  if current_user == @comment.user.name
+	
 			@post = Post.find(params[:post_id])
 			@comment = @post.comments.find(params[:id])
-			@comment.destroy
-			redirect_to post_path(@post)
-	  else
-		  	redirect_to root_path
-	  end
+			if current_user == @comment.user
+				@comment.destroy
+				redirect_to post_path(@post)
+			  else
+			  	redirect_to posts_path
+		  	end
 	end
 end
