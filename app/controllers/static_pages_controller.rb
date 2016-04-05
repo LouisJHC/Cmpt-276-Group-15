63e748292aef7b7845f849eeb32beee5d936dd65
DukeZheng <zjtduke@hotmail.com>
2016-04-05 00:37:52 +0000

    stable version of tracker

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -23,4 +23,7 @@ class StaticPagesController < ApplicationController
   def locations
   end
   
+  def trackers
+  end
+  
 end
--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,2 +1,11 @@
 class Tracker < ActiveRecord::Base
+    validates :activity, presence: true
+    validates :duration, presence: true
+    validates :duration, numericality: true
+    validates :effect, presence: true
+    validates :weight, presence: true
+    validates :weight, numericality: true
+    validates :totolduration, presence: true
+    validates :totolduration, numericality: true
+    validates :progress, presence: true
 end
